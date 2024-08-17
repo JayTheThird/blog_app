9ef7c7ae3f7decfca59b72dfb2832edb7504442c
JayTheThird <jaypatel161200@gmail.com>
2024-08-17 18:18:07 +0530

    created database table called user_profiles in supabase and that table is connected to auth, when new user is created it also added this user in user_profiles table via trigger

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,5 +1,5 @@
 class AppSecrets {
-  static const supabaseUrl = "https://oqjbapeltshdcsldtpdo.supabase.co";
+  static const supabaseUrl = "https://oyafmwhkmbmmaowdtwlt.supabase.co";
   static const supabaseAnonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xamJhcGVsdHNoZGNzbGR0cGRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2OTc0MzQsImV4cCI6MjAzOTI3MzQzNH0.A0Htv4AhUuPaMb7B6NoegPMz9xkPEFSNQrrimtglOXU";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95YWZtd2hrbWJtbWFvd2R0d2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM4OTgzNDIsImV4cCI6MjAzOTQ3NDM0Mn0.XYGunQ9cUjvtruORQTjBhVDQmjVH9eSGBcnbuVM_ASk";
 }
